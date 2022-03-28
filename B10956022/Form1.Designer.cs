727850360e8075e43d934d0b96662e60d5ad82f5
user <user@DESKTOP-6JBEF5K>
2022-03-28 11:51:05 +0800

    3/28-1

--- B10956022/Form1.Designer.cs
+++ B10956022/Form1.Designer.cs
@@ -162,6 +162,7 @@ namespace B10956022
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- B10956022/Form1.cs
+++ B10956022/Form1.cs
@@ -69,5 +69,25 @@ namespace B10956022
             S.Send(B, B.Length, IP, Port);
             S.Close();
         }
+
+
+        private string MyIP()
+        {
+            string hostname = Dns.GetHostName();
+            IPAddress[] ip = Dns.GetHostEntry(hostname).AddressList;
+            foreach(IPAddress it in ip)
+            {
+                if (it.AddressFamily == AddressFamily.InterNetwork&& it.ToString()!="192.168.56.1")
+                {
+                    return it.ToString();
+                }
+            }
+            return "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "我的IP:" + MyIP();
+        }
     }
 }
